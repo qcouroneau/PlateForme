6e1cef71a04043e6580779165d2d0243e8e3821b
adisse1807 <a.disse@outlook.fr>
2022-03-28 15:36:20 +0200

    Et rebelotte

--- FrontPlateforme/src/app/app-routing.module.ts
+++ FrontPlateforme/src/app/app-routing.module.ts
@@ -3,16 +3,16 @@ import { RouterModule, Routes } from "@angular/router";
 import { CreateProjectComponent } from "./create-project/create-project.component";
 import { HomeComponent } from "./home/home.component";
 
-const routs : Routes = [
-    {path: "home", component: HomeComponent},
-    {path: "newproject", component: CreateProjectComponent},
-    
-    {path: "", redirectTo: "home", pathMatch: "full"},
-    {path: "**", redirectTo: "home", pathMatch: "full"}
+const routes : Routes = [
+    { path: "home", component: HomeComponent },
+    { path: "newproject", component: CreateProjectComponent },
+
+    { path: "", redirectTo: "home", pathMatch: "full" },
+    { path: "**", redirectTo: "home", pathMatch: "full" }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routs)],
+    imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
 
