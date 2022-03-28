90684da2159c8d36dec52b8c0df2ce69eaefee41
j-dubert <j.dubert12@gmail.com>
2022-03-28 14:48:43 +0200

    add button and page

---
+++ FrontPlateforme/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CreateProjectComponent } from "./create-project/create-project.component";
+import { HomeComponent } from "./home/home.component";
+
+const routs : Routes = [
+    {
+        path: "home", component :HomeComponent
+    },
+    {
+        path: "newproject", component: CreateProjectComponent
+    },
+    {
+        path: "", redirectTo: "home", pathMatch: "full"
+    },
+    {
+        path: "**", redirectTo: "home", pathMatch: "full"
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routs)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
--- FrontPlateforme/src/app/app.module.ts
+++ FrontPlateforme/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CreateProjectComponent } from './create-project/create-project.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CreateProjectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
