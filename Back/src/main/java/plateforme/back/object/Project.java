1155ab2730306cc07d9206b6ca167d58810a11c5
Chad Benaaros <chad.benaaros33@gmail.com>
2022-03-31 15:25:10 +0200

    fix de l'oubli d'un import

--- Back/src/main/java/plateforme/back/object/Project.java
+++ Back/src/main/java/plateforme/back/object/Project.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
