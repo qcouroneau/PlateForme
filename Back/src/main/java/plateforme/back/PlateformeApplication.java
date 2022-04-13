204b62983b2d2568fb7c58eb62cd7b1017c1b55d
Chad Benaaros <chad.benaaros33@gmail.com>
2022-04-13 09:48:18 +0200

    fix cors

--- Back/src/main/java/plateforme/back/PlateformeApplication.java
+++ Back/src/main/java/plateforme/back/PlateformeApplication.java
@@ -3,7 +3,6 @@ package plateforme.back;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,16 +16,10 @@ public class PlateformeApplication {
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedHeaders("*")
-						.allowedMethods("*").allowCredentials(true);
+			public void addResourceHandlers(ResourceHandlerRegistry registry) {
+				registry.addResourceHandler("/project-photos/**").addResourceLocations("file:project-photos/")
+						.setCachePeriod(0);
 			}
-			
-		    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		        registry.addResourceHandler("/project-photos/**").addResourceLocations("file:project-photos/")
-		                .setCachePeriod(0);
-		    }
 		};
 	}
 }
--- Back/src/main/java/plateforme/back/configuration/SecurityConfiguration.java
+++ Back/src/main/java/plateforme/back/configuration/SecurityConfiguration.java
@@ -11,9 +11,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import plateforme.back.filters.AuthTokenFilter;
+import plateforme.back.filters.CorsFilterConfig;
 import plateforme.back.service.UserDetailsServiceImpl;
 
 @Configuration
@@ -43,7 +45,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests().antMatchers("/project/create").authenticated().anyRequest()
 				.permitAll();
+		http.addFilterBefore(new CorsFilterConfig(), ChannelProcessingFilter.class);
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+		http.cors();
 	}
 
 	@Override
