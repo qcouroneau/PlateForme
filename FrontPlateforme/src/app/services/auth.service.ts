6a55fa9ce9f2f830a546d792c701f713d3442e27
elbadre.ib@gmail.com <Pattern123!>
2022-04-14 14:51:56 +0200

    guard auth token

--- FrontPlateforme/src/app/services/auth.service.ts
+++ FrontPlateforme/src/app/services/auth.service.ts
@@ -1,23 +1,24 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { IUser } from '../entities/user-reference';
-import { environment } from "src/environments/environment";
-import { urls } from "src/urls";
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {IUser} from '../entities/user-reference';
+import {environment} from "src/environments/environment";
+import {urls} from "src/urls";
 import {ICredential} from "../entities/credential-reference";
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 
 export class AuthService {
 
   private loggedUserSubject: BehaviorSubject<IUser>;
   public loggedInUser: Observable<any>;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, public jwtHelper: JwtHelperService) {
   }
 
   login(credentials: ICredential): Observable<any> {
@@ -27,4 +28,10 @@ export class AuthService {
   register(user: IUser): Observable<any> {
     return this.http.post(environment.apiUrl + urls.user.register, user, httpOptions);
   }
+
+  public isAuthenticated(): boolean {
+    const token = sessionStorage.getItem('auth-token');    // Check whether the token is expired and return
+    return !this.jwtHelper.isTokenExpired(token);
+  }
+
 }
